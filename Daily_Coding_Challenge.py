82257a50581b45acd657206e322e9b57aface728
Michael Mbajwa <mac@MACs-MacBook-Pro.local>
2021-11-08 09:38:15 +0100

    New coding challenge added

--- Daily_Coding_Challenge.py
+++ Daily_Coding_Challenge.py
@@ -656,3 +656,21 @@ def prod_arr(arr):
         new_arr.append(prod)
 
     return new_arr
+
+
+def merge_sorted(arr):
+    """
+    Return a new sorted merged list from K sorted lists, each with size N.
+    For example, if we had [[10, 15, 30], [12, 15, 20], [17, 20, 32]], the result should be
+    [10, 12, 15, 15, 17, 20, 20, 30, 32].
+    """
+    # Naive solution
+    merged_list = list()
+    for inn_arr in arr:
+        for i in inn_arr:
+            merged_list.append(i)
+
+    return sorted(merged_list)
+
+
+print(merge_sorted([[10, 15, 30], [12, 15, 20], [17, 20, 32]]))
