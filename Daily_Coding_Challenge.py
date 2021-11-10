13c7d6fcc303214300c481ccb85f5d82602e913d
Michael Mbajwa <mac@MACs-MacBook-Pro.local>
2021-11-10 11:14:26 +0100

    Coding challenge for 9 Nov 2021

--- Daily_Coding_Challenge.py
+++ Daily_Coding_Challenge.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import heapq
 
 
 def is_multiple(a, b):
@@ -706,3 +707,63 @@ def check_sum(value):
         except TypeError:
             pass
     return main_sum
+
+
+"""
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s),
+which deserializes the string back into the tree.
+
+For example, given the following Node class
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+The following test should pass:
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+"""
+
+
+def first_missing_integer_wrong(arr):
+    # This is not a correct solution for this particular problem. This code only works if we are to work within the
+    # interval of (min-1, max+1) of the arr. For example an arr like [15, 14, 13, 11] will give 10 instead of 0.
+    """
+    Given an array of integers, find the first missing positive integer in linear time and constant space.
+    In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates
+    and negative numbers as well.
+
+    For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+    You can modify the input array in-place.
+    """
+    my_heap = []
+    heapq.heapify(my_heap)
+    for i in arr:
+        if (i-1) > 0 and (i-1) not in arr and (i-1) not in my_heap:
+            heapq.heappush(my_heap, i-1)
+        if (i+1) > 0 and (i+1) not in arr and (i+1) not in my_heap:
+            heapq.heappush(my_heap, i+1)
+    return heapq.heappop(my_heap)
+
+
+def first_missing_int(arr):
+    # I believe this is a more appropriate solution
+    """
+    Given an array of integers, find the first missing positive integer in linear time and constant space.
+    In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates
+    and negative numbers as well.
+
+    For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+    You can modify the input array in-place.
+    """
+    for i in range(1, max(arr)+2):
+        try:
+            arr.index(i)
+        except ValueError:
+            return i
+
