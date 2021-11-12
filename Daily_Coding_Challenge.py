b81daabbc9a0092f800a425d66b4baa485e32cfd
Michael Mbajwa <mac@MACs-MacBook-Pro.local>
2021-11-12 09:41:06 +0100

    Coding challenge on voter_fraud

--- Daily_Coding_Challenge.py
+++ Daily_Coding_Challenge.py
@@ -810,6 +810,28 @@ car(cons(3, 4))
 cdr(cons(3, 4))
 
 
+def voter_fraud(arr_2d):
+    """
+    Given a two dimensional list of integers votes, where each list has two elements [candidate_id, voter_id],
+    report whether any voter has voted more than once.
+    Constraints:
+    votes = [
+    [3, 1],
+    [3, 0],
+    [3, 4],
+    [3, 3],
+    [3, 2]]. Result = False
+    n ≤ 100,000 where n is the length of votes
+    """
+    test = set()
+    for arr in arr_2d:
+        if arr[1] in test:
+            return True
+        else:
+            test.add(arr[1])
+    return False
+
+
 def xor_linked_list():
     """
     An XOR linked list is a more memory efficient doubly linked list. Instead of each node holding next and prev fields,
