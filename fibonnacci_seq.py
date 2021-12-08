d3cd507d81331e0f739c384812a40473f4ca4d7c
Michael Mbajwa <mac@MACs-MacBook-Pro.local>
2021-12-08 10:39:17 +0100

    fib_seq

--- fibonnacci_seq.py
+++ fibonnacci_seq.py
@@ -1,13 +1,15 @@
 # Solved using dynamic programing
 def fib(n):
-    seq = [0] * n
-    seq[0], seq[1] = 1, 1
+    if n == 0:
+        return 0
     if n == 1 or n == 2:
         return 1
+    seq = [0] * n
+    seq[0], seq[1] = 1, 1
     for i in range(2, n):
         seq[i] = seq[i-1] + seq[i-2]
     return seq[n-1]
 
 
 if __name__ == "__main__":
-    print(fib(100))
+    print(fib(3))
