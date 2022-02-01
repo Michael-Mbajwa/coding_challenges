604f38428088fa0b08212ba732c5fb2bbc4603fe
Michael Mbajwa <mac@MACs-MacBook-Pro.local>
2022-02-01 11:38:32 +0100

    print stars

---
+++ print_stars.py
@@ -0,0 +1,13 @@
+def stars(n):
+    nrows = n * 2 - 1
+    star = "*"
+    n_steps = 1
+    while n_steps < nrows:
+        total_space = nrows - n_steps
+        space_left = total_space//2
+        space_right = space_left
+        print(" " * space_left + star * n_steps + " " * space_right)
+        n_steps += 2
+
+
+stars(10)
